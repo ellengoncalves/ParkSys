e9a7c92e7d72adfb78f5284835d14add291a2d14
ellengoncalves <ellenpingon@gmail.com>
2026-06-06 10:31:13 -0300

    feat(M01): criar EntradaRunnable

---
+++ src/parksys/services/EntradaRunnable.java
@@ -0,0 +1,26 @@
+package parksys.services;
+
+import parksys.enums.TipoVeiculo;
+
+public class EntradaRunnable implements Runnable {
+    private final String placa;
+    private final TipoVeiculo tipoVeiculo;
+    private final String idVagaDesejada;
+    private final GerenciadorEstacionamento gerenciadorEstacionamento;
+
+    public EntradaRunnable(
+            String placa,
+            TipoVeiculo tipoVeiculo,
+            String idVagaDesejada,
+            GerenciadorEstacionamento gerenciadorEstacionamento) {
+        this.placa = placa;
+        this.tipoVeiculo = tipoVeiculo;
+        this.idVagaDesejada = idVagaDesejada;
+        this.gerenciadorEstacionamento = gerenciadorEstacionamento;
+    }
+
+    @Override
+    public void run() {
+        gerenciadorEstacionamento.registrarEntrada(placa, tipoVeiculo, idVagaDesejada);
+    }
+}
